0e26a0c72823e5f2083dc6587b019c4f98b88bdc
Çiğdem İrgin <cigdemirgin@gmail.com>
2024-03-17 01:33:06 +0300

    fix missing key prop

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function PhotoList() {
       role="list"
       className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8"
     >
-      {files.map((item) => (
-        <PhotoListItem file={item} />
+      {files.map((item,index) => (
+        <PhotoListItem key={index} file={item} />
       ))}
     </ul>
   );
